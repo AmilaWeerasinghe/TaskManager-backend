13804d8487cecca43bb4ae6097357e76941bb2fa
AmilaWeerasinghe <amilaweerasinghe677@gmail.com>
2023-11-11 10:43:48 +0530

    enable cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // index.js
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5001;
 const taskRoutes = require('./routes/tasks');
 
+app.use(cors());
 app.use(express.json());
 app.use('/tasks', taskRoutes);
 
